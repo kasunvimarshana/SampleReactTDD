3061bf848cd8eac46c8e2b128c03942b9c7e4d1d
kasunvimarshana <kasunvmail@gmail.com>
2023-05-21 01:36:31 +0530

    add home tests

--- src/components/Homes.js
+++ src/components/Homes.js
@@ -1,29 +1,11 @@
 import React, {useState, useEffect} from 'react';
+import apiClient from '../sevices/apiClient';
 
 export default function Homes() {
     const [homesState, setHomesState] = useState([]);
 
     useEffect(() => {
-        const homesDataPromise = Promise.resolve([
-            {
-              title: "Test home 1",
-              image: "listing.jpg",
-              location: "Test location 1",
-              price: "1",
-            },
-            {
-              title: "Test home 2",
-              image: "listing.jpg",
-              location: "Test location 2",
-              price: "2",
-            },
-            {
-              title: "Test home 3",
-              image: "listing.jpg",
-              location: "Test location 3",
-              price: "3",
-            }
-        ]);
+        const homesDataPromise = apiClient.getHomes();
 
         homesDataPromise.then(homesData => setHomesState(homesData));
     }, []);
@@ -32,7 +14,12 @@ export default function Homes() {
 
     homes = homesState.map((home, index) => {
         return (
-            <div data-testid="home" key={ index }>Home!</div>
+            <div data-testid="home" key={ index }>
+                <div data-testid="home-title">{home.title}</div>
+                <img data-testid="home-image" src={home.image} alt={home.title}/>
+                <div data-testid="home-location">{home.location}</div>
+                <div data-testid="home-price">{home.price}</div>
+            </div>
         );
     });
       
--- src/components/Homes.test.js
+++ src/components/Homes.test.js
@@ -1,10 +1,34 @@
 import React from 'react';
-import {act, getAllByTestId, render} from '@testing-library/react';
+import {act, getAllByTestId, getNodeText, render} from '@testing-library/react';
 import Homes from './Homes';
+import apiClient from '../sevices/apiClient';
 
 let container = null;
 
 beforeEach(async () => {
+    jest.spyOn(apiClient, 'getHomes').mockImplementation(() => {
+        const homesDataPromise = Promise.resolve([
+            {
+              title: "Test home 1",
+              image: "listing.jpg",
+              location: "Test location 1",
+              price: "1",
+            },
+            {
+              title: "Test home 2",
+              image: "listing.jpg",
+              location: "Test location 2",
+              price: "2",
+            },
+            {
+              title: "Test home 3",
+              image: "listing.jpg",
+              location: "Test location 3",
+              price: "3",
+            }
+        ]);
+        return homesDataPromise;
+    });
     container = render(<Homes/>).container;
     await act(async () => {});
 });
@@ -16,4 +40,24 @@ it('test', () => {
 it('should show homes', () => {
     const homes = getAllByTestId(container, 'home');
     expect(homes.length).toBeGreaterThan(0);
+});
+
+it('should show home title', () => {
+    const homeTitles = getAllByTestId(container, 'home-title');
+    expect(getNodeText(homeTitles[0])).toBe('Test home 1');
+});
+
+it('should show home image', () => {
+    const homeImages = getAllByTestId(container, 'home-image');
+    expect(homeImages[0]).toBeTruthy();
+});
+
+it('should show home location', () => {
+    const homeLocations = getAllByTestId(container, 'home-location');
+    expect(getNodeText(homeLocations[0])).toBe('Test location 1');
+});
+
+it('should show home price', () => {
+    const homePrices = getAllByTestId(container, 'home-price');
+    expect(getNodeText(homePrices[0])).toBe('1');
 });
